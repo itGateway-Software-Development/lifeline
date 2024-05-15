7941208e2c2c7b6016b9b362cbf2854019b8e808
itGateway-Software-Development <developer@itgateway.org>
2024-05-15 13:28:08 +0630

    loading

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,21 +3,37 @@
     <Carousel></Carousel>
     <Welcome></Welcome>
     <Activity></Activity>
+
+    <Loading v-if="isLoading"></Loading>
   </div>
 </template>
 
 <script>
+import Loading from '../components/Loading'
 import Activity from "../components/Activity";
 import Welcome from "../components/Welcome";
 import Carousel from "../components/Carousel";
+import { onMounted, ref } from 'vue';
 // @ is an alias to /src
 
 export default {
   components: {
+    Loading,
     Activity,
     Welcome,
     Carousel,
   },
   name: "HomeView",
+  setup() {
+    const isLoading = ref(true);
+
+    onMounted(() => {
+      window.addEventListener('load', () => {
+        isLoading.value = false;
+      })
+    })
+
+    return {isLoading}
+  }
 };
 </script>
